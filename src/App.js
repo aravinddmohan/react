0a925b9bb72a74c00601d8027d63c8cc27113633
tambiarav <1aravindmohan@gmail.com>
2025-12-23 21:44:19 +0530

    idea 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import HobbyCard from "./components/HobbyCard";
+import {BrowserRouter,Routes,Route } from "react-router-dom";
 import WelcomeCard from "./components/WelcomeCard";
+import HobbyCard from "./components/HobbyCard";
+import FavFood from "./components/FavFood";
 
 function App() {
   return (
-    <div>
-      <WelcomeCard />
-      <HobbyCard />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomeCard />} />
+        <Route path="/hobbies" element={<HobbyCard />} />
+        <Route path="/foods" element={<FavFood />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+} 
 
 export default App;
---
+++ src/components/FavFood.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+export default function FavFood(){
+    const foods = ["watermelon","apple","hotdogs","sandwich","lemon"];
+    const [message,setMessage] = useState("click any food");
+    const handleClick =(food) => {
+        setMessage(`i love ${food}`);
+    };
+return (
+    <div>
+        <h2>
+            My Favorite Foods
+        </h2>
+        <ul>
+            {foods.map
+        ((food,index) => 
+            (
+               <li key={index}>
+                {food}
+                <button onClick={() => handleClick(food)} style={{marginLeft:"10px"}}>Like</button>
+               </li>
+            )
+        )
+            }
+        </ul>
+        <p>{message}</p>
+    </div>
+);
+}
